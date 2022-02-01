7401456474aa01dbff79b50d8c8e6233b1aa1cb5
Fer Sánchez <sangarfer@gmail.com>
2022-02-01 13:51:58 +0100

    menu improvements

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
-const canvas_x = canvas.getBoundingClientRect().left;
 
 const background = new Background(ctx);
 const bricks = new Bricks(ctx);
@@ -14,20 +13,27 @@ const game = new Game(ctx, background, bricks, player, ball);
 
 
 const startButton = document.getElementById("start-button");
-const title = document.getElementById("title");
+const gameboard = document.getElementById("gameboard");
+const menu = document.getElementById("menu");
+const general = document.getElementById("general");
+
+let canvasX;
 
 startButton.onclick = ()=> {
   startButton.blur();
   canvas.focus();
-  startButton.remove();
-  title.remove();
+  gameboard.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
   game.startGame();
+  canvasX = gameboard.getBoundingClientRect().left;
 };
 
 canvas.addEventListener(
   "mousemove",
   (event)=>{
-      player.x = event.clientX - canvas_x - (player.width / 2);
+      player.x = event.clientX - canvasX - (player.width / 2);
+      console.log("event: ", event.clientX)
+      console.log("player: ", player.x)
       if(player.x <= 0){player.x = 0};
       if(player.x + player.width > ctx.canvas.width){player.x = ctx.canvas.width - player.width};
       if(game.gameState === 0){ball.x = player.x + player.width / 2};
